cea280c5ef09a58abcc79fb379e2284ec6935094
bennysonson <epiccowslayer@gmail.com>
2020-07-14 21:02:25 -0400

    Added prject

---
+++ variables.js
@@ -0,0 +1,47 @@
+/** Alphabet for tier labels */
+var alphabet = [
+    'S',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G'
+]
+
+/** Colors for tier labels */
+var labelColors = [
+    '#FE0101',
+    '#12FE01',
+    '#0D46FE',
+    '#FEFE01',
+    '#FE6FDD',
+    '#8a7af0',
+    '#b9f07a'
+
+]
+
+/** Placeholder ids for new tiers */
+var newTiers = [
+    'newTier-2',
+    'newTier-1',
+    'newTier0',
+    'newTier1',
+    'newTier2',
+    'newTier3',
+    'newTier4',
+    'newTier5',
+]
+
+/** Placeholder ids for new tier items */
+var newTierItems = [
+    'newTier-2Items',
+    'newTier-1Items',
+    'newTier0Items',
+    'newTier1Items',
+    'newTier2Items',
+    'newTier3Items',
+    'newTier4Items',
+    'newTier5Items',
+]
